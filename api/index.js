5c6e046b10686208f3e5969098388253e205cdb4
Kunal Kumar <102505903+Kunalkr08@users.noreply.github.com>
2024-04-16 21:20:07 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -46,37 +46,37 @@ app.get("/", (req, res)=>{
   res.json("server");
 })
 
-// app.post('/register', async (req,res) => {
-//   const {username,password} = req.body;
-//   try{
-//     const userDoc = await User.create({
-//       username,
-//       password:bcrypt.hashSync(password,salt),
-//     });
-//     res.json(userDoc);
-//   } catch(e) {
-//     console.log(e);
-//     res.status(400).json(e);
-//   }
-// });
+app.post('/register', async (req,res) => {
+  const {username,password} = req.body;
+  try{
+    const userDoc = await User.create({
+      username,
+      password:bcrypt.hashSync(password,salt),
+    });
+    res.json(userDoc);
+  } catch(e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
+});
 
-// app.post('/login', async (req,res) => {
-//   const {username,password} = req.body;
-//   const userDoc = await User.findOne({username});
-//   const passOk = bcrypt.compareSync(password, userDoc.password);
-//   if (passOk) {
-//     // logged in
-//     jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
-//       if (err) throw err;
-//       res.cookie('token', token).json({
-//         id:userDoc._id,
-//         username,
-//       });
-//     });
-//   } else {
-//     res.status(400).json('wrong credentials');
-//   }
-// });
+app.post('/login', async (req,res) => {
+  const {username,password} = req.body;
+  const userDoc = await User.findOne({username});
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    // logged in
+    jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+      if (err) throw err;
+      res.cookie('token', token).json({
+        id:userDoc._id,
+        username,
+      });
+    });
+  } else {
+    res.status(400).json('wrong credentials');
+  }
+});
 
 // app.get('/profile', (req,res) => {
 //   const {token} = req.cookies;
