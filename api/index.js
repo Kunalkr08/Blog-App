f9b0f7fc8d319cd186c454fa18157b29461d9af1
Kunal Kumar <102505903+Kunalkr08@users.noreply.github.com>
2024-04-16 21:10:57 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -46,49 +46,49 @@ app.get("/", (req, res)=>{
   res.json("server");
 })
 
-app.post('/register', async (req,res) => {
-  const {username,password} = req.body;
-  try{
-    const userDoc = await User.create({
-      username,
-      password:bcrypt.hashSync(password,salt),
-    });
-    res.json(userDoc);
-  } catch(e) {
-    console.log(e);
-    res.status(400).json(e);
-  }
-});
+// app.post('/register', async (req,res) => {
+//   const {username,password} = req.body;
+//   try{
+//     const userDoc = await User.create({
+//       username,
+//       password:bcrypt.hashSync(password,salt),
+//     });
+//     res.json(userDoc);
+//   } catch(e) {
+//     console.log(e);
+//     res.status(400).json(e);
+//   }
+// });
 
-app.post('/login', async (req,res) => {
-  const {username,password} = req.body;
-  const userDoc = await User.findOne({username});
-  const passOk = bcrypt.compareSync(password, userDoc.password);
-  if (passOk) {
-    // logged in
-    jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
-      if (err) throw err;
-      res.cookie('token', token).json({
-        id:userDoc._id,
-        username,
-      });
-    });
-  } else {
-    res.status(400).json('wrong credentials');
-  }
-});
+// app.post('/login', async (req,res) => {
+//   const {username,password} = req.body;
+//   const userDoc = await User.findOne({username});
+//   const passOk = bcrypt.compareSync(password, userDoc.password);
+//   if (passOk) {
+//     // logged in
+//     jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+//       if (err) throw err;
+//       res.cookie('token', token).json({
+//         id:userDoc._id,
+//         username,
+//       });
+//     });
+//   } else {
+//     res.status(400).json('wrong credentials');
+//   }
+// });
 
-app.get('/profile', (req,res) => {
-  const {token} = req.cookies;
-  jwt.verify(token, secret, {}, (err,info) => {
-    if (err) throw err;
-    res.json(info);
-  });
-});
+// app.get('/profile', (req,res) => {
+//   const {token} = req.cookies;
+//   jwt.verify(token, secret, {}, (err,info) => {
+//     if (err) throw err;
+//     res.json(info);
+//   });
+// });
 
-app.post('/logout', (req,res) => {
-  res.cookie('token', '').json('ok');
-});
+// app.post('/logout', (req,res) => {
+//   res.cookie('token', '').json('ok');
+// });
 
 
 // app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
@@ -149,21 +149,21 @@ app.post('/logout', (req,res) => {
 
 // });
 
-app.get('/post', async (req,res) => {
-  res.json(
-    await Post.find()
-      .populate('author', ['username'])
-      .sort({createdAt: -1})
-      .limit(20)
-  );
-});
+// app.get('/post', async (req,res) => {
+//   res.json(
+//     await Post.find()
+//       .populate('author', ['username'])
+//       .sort({createdAt: -1})
+//       .limit(20)
+//   );
+// });
 
 
-app.get('/post/:id', async (req, res) => {
-  const {id} = req.params;
-  const postDoc = await Post.findById(id).populate('author', ['username']);
-  res.json(postDoc);
-})
+// app.get('/post/:id', async (req, res) => {
+//   const {id} = req.params;
+//   const postDoc = await Post.findById(id).populate('author', ['username']);
+//   res.json(postDoc);
+// })
 
 
 
